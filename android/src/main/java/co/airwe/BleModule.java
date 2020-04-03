5c93dcfcf2b150f0b95e1fa7c2564c997a5d4792
Federico Kattan <fkattan@gmail.com>
2020-04-03 16:33:49 -0300

    WIP

--- android/src/main/java/co/airwe/BleModule.java
+++ android/src/main/java/co/airwe/BleModule.java
@@ -12,15 +12,18 @@ import android.content.Intent;
 import android.content.ServiceConnection;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.RemoteException;
 import android.util.Log;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.LifecycleEventListener;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
 
 import org.altbeacon.beacon.BeaconConsumer;
 import org.altbeacon.beacon.BeaconManager;
+import org.altbeacon.beacon.Region;
 
 
 public class BleModule extends ReactContextBaseJavaModule implements LifecycleEventListener, ActivityEventListener, BeaconConsumer{
@@ -29,7 +32,12 @@ public class BleModule extends ReactContextBaseJavaModule implements LifecycleEv
 
     private static final int PERMISSION_REQUEST_FINE_LOCATION = 1;
 
-    private final ReactApplicationContext reactContext;
+    private static final int BLE_SCAN_PERIOD = 1100; // 1.1s
+    private static final int BLE_SCAN_FREQUENCY = 60000; // 1min
+    private static final String BLE_REGION_ID = "co.airwe.region";
+
+
+  private final ReactApplicationContext reactContext;
     private BeaconManager mBeaconManager;
 
     public BleModule(ReactApplicationContext reactContext) {
@@ -45,6 +53,63 @@ public class BleModule extends ReactContextBaseJavaModule implements LifecycleEv
     @Override
     public String getName() {
         return "Ble";
+    }
+
+
+
+    @ReactMethod
+    public void startScanning() {
+      Notification.Builder builder = new Notification.Builder(reactContext);
+
+      builder.setSmallIcon(android.R.mipmap.ic_launcher);
+      builder.setContentTitle("Scanning for Beacons");
+
+      //Intent intent = new Intent(this, getCurrentActivity().getClass());
+      String pn = getReactApplicationContext().getApplicationContext().getPackageName();
+      Intent intent = reactContext.getPackageManager().getLaunchIntentForPackage(pn);
+      PendingIntent pendingIntent = PendingIntent.getActivity(
+        reactContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
+      );
+      builder.setContentIntent(pendingIntent);
+
+      if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
+          "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
+        channel.setDescription("My Notification Channel Description");
+        NotificationManager notificationManager = (NotificationManager) reactContext.getSystemService(
+          Context.NOTIFICATION_SERVICE
+        );
+        notificationManager.createNotificationChannel(channel);
+        builder.setChannelId(channel.getId());
+      }
+
+      mBeaconManager.enableForegroundServiceScanning(builder.build(), 456);
+      // For the above foreground scanning service to be useful, you need to disable
+      // JobScheduler-based scans (used on Android 8+) and set a fast background scan
+      // cycle that would otherwise be disallowed by the operating system.
+      //
+      mBeaconManager.setEnableScheduledScanJobs(false);
+      mBeaconManager.setBackgroundBetweenScanPeriod(BLE_SCAN_FREQUENCY);
+      mBeaconManager.setBackgroundScanPeriod(BLE_SCAN_PERIOD);
+
+      try {
+        mBeaconManager.startMonitoringBeaconsInRegion(new Region(BLE_REGION_ID, null, null, null));
+      } catch (RemoteException e) {
+        Log.e(TAG, "Error trying to monitor Region: " + BLE_REGION_ID);
+        e.printStackTrace();
+      }
+
+    }
+
+
+    @ReactMethod
+    public void stopScanning() {
+      try {
+        mBeaconManager.stopMonitoringBeaconsInRegion(new Region(BLE_REGION_ID, null, null, null));
+      } catch (RemoteException e) {
+        Log.e(TAG, "Error trying to stop monitoring Region: " + BLE_REGION_ID);
+        e.printStackTrace();
+      }
     }
 
     @Override
@@ -81,13 +146,6 @@ public class BleModule extends ReactContextBaseJavaModule implements LifecycleEv
     @Override
     public void onHostDestroy() {
 
-      // Stop Scan Service
-      Intent bleScanServiceIntent = new Intent(reactContext, BleScanService.class);
-      reactContext.getApplicationContext().stopService(bleScanServiceIntent);
-
-      // Stop Advertising Service
-      Intent bleAdvertisingService = new Intent(reactContext, BleAdvertiserService.class);
-      reactContext.getApplicationContext().stopService(bleAdvertisingService);
     }
 
   @Override
@@ -100,38 +158,6 @@ public class BleModule extends ReactContextBaseJavaModule implements LifecycleEv
   @Override
   public void onBeaconServiceConnect() {
 
-    Notification.Builder builder = new Notification.Builder(reactContext);
-
-    builder.setSmallIcon(android.R.mipmap.ic_launcher);
-    builder.setContentTitle("Scanning for Beacons");
-
-    //Intent intent = new Intent(this, getCurrentActivity().getClass());
-    String pn = getReactApplicationContext().getApplicationContext().getPackageName();
-    Intent intent = reactContext.getPackageManager().getLaunchIntentForPackage(pn);
-    PendingIntent pendingIntent = PendingIntent.getActivity(
-      reactContext, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT
-    );
-    builder.setContentIntent(pendingIntent);
-
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-      NotificationChannel channel = new NotificationChannel("My Notification Channel ID",
-        "My Notification Name", NotificationManager.IMPORTANCE_DEFAULT);
-      channel.setDescription("My Notification Channel Description");
-      NotificationManager notificationManager = (NotificationManager) reactContext.getSystemService(
-        Context.NOTIFICATION_SERVICE
-      );
-      notificationManager.createNotificationChannel(channel);
-      builder.setChannelId(channel.getId());
-    }
-
-    mBeaconManager.enableForegroundServiceScanning(builder.build(), 456);
-    // For the above foreground scanning service to be useful, you need to disable
-    // JobScheduler-based scans (used on Android 8+) and set a fast background scan
-    // cycle that would otherwise be disallowed by the operating system.
-    //
-    mBeaconManager.setEnableScheduledScanJobs(false);
-    mBeaconManager.setBackgroundBetweenScanPeriod(0);
-    mBeaconManager.setBackgroundScanPeriod(1100);
   }
 
   @Override
