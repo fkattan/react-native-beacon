6c751781456786271807f836e095feef766e15ac
Federico Kattan <fkattan@gmail.com>
2020-04-07 18:51:12 -0300

    Updated ts types

--- index.d.ts
+++ index.d.ts
@@ -2,5 +2,7 @@ declare module 'react-native-ble' {
 
     export function startMonitoringRegion():void;
     export function stopMonitoringRegion():void;
+    export function startRangingInRegion():void;
+    export function stopRangingInRegion():void;
     export function initializeBeaconManager():void;
 }
